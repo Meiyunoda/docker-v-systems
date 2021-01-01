acc5b0d3512c2254ccc963d453c5f9b63f19f5e0
Sunny King <sunnyking9999@gmail.com>
2021-01-01 15:45:02 +0000

    Add to docker group so no need to sudo for docker command

--- aws/bootstrap.sh
+++ aws/bootstrap.sh
@@ -3,6 +3,7 @@
 
 sudo apt update
 sudo apt install -y docker.io
+sudo usermod -aG docker ubuntu
 
 if [ -d ~/ssd ]; then
     echo "SSD mount already exists, bootstrapped already?"
@@ -16,3 +17,6 @@ sudo mkfs.ext4 /dev/nvme1n1
 echo "/dev/nvme1n1 /home/ubuntu/ssd auto noatime 0 0" | sudo tee -a /etc/fstab
 
 sudo mount -a
+
+# Need to re-login to allow group membership be acknowledged
+echo "Bootstrap done, please exit the ssh session and login again."
